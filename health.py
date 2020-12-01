657658cc66a729a1a733adb9b81fad894b264b4d
Gamererr <starkid2u@outlook.com>
2020-12-01 08:15:31 -0500

    made stands folder, only part 3 though

---
+++ health.py
@@ -0,0 +1,216 @@
+import discord
+import asyncio
+import random
+from discord.ext import commands
+
+async def kill(killed, healthroles):
+	await killed.remove_roles(healthroles[1], reason="killed")
+	await killed.add_roles(healthroles[0], reason="killed")
+	await asyncio.sleep(3600)
+	await killed.remove_roles(healthroles[0], reason="revived")
+	await killed.add_roles(healthroles[1], reason="revived")
+
+@commands.cooldown(1, random.randrange(3, 6), commands.BucketType.user)
+async def standattack(attacker, attacked, dmg, healthroles, message):
+	stand = discord.utils.get(message.guild.roles, id = 712718404901142528)
+	dead_visitor = discord.utils.get(message.guild.roles, id = 747460046333673573)
+	rof_armour = discord.utils.get(message.guild.roles, id = 747465352841134190)
+	pantarmour = discord.utils.get(message.guild.roles, id = 751637329088610365)
+	scared = discord.utils.get(message.guild.roles, id = 749429015621664790)
+	standattackcooldown = discord.utils.get(message.guild.roles, id = 749764783498199080)
+	hamonattackcooldown = discord.utils.get(message.guild.roles, id = 749767003379728466)
+	hamonhealcooldown = discord.utils.get(message.guild.roles, id = 749768438418898975)
+	enraged = discord.utils.get(message.guild.roles, id = 749824364618186844)
+	enraged_victim = discord.utils.get(message.guild.roles, id = 749824617648226326)
+
+	
+	if healthroles[0] in message.author.roles or healthroles[0] in message.mentions[0].roles or scared in message.author.roles:
+		return
+	if stand in attacked.roles:
+		await message.channel.send("attack the user, not the stand")
+		return
+	if enraged in attacked.roles:
+		message.channel.send("user is in a blind rage, you cannot damage them")
+		return
+	if enraged_victim in attacked.roles and enraged in attacker.roles:
+		dmg = dmg*2
+	for x in range(dmg):
+		if (healthroles[10] in attacked.roles):
+			await attacked.remove_roles(healthroles[10],reason="attacked")
+			await attacked.add_roles(healthroles[9],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[9] in attacked.roles):
+			await attacked.remove_roles(healthroles[9],reason="attacked")
+			await attacked.add_roles(healthroles[8],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[8] in attacked.roles):
+			await attacked.remove_roles(healthroles[8],reason="attacked")
+			await attacked.add_roles(healthroles[7],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[7] in attacked.roles):
+			await attacked.remove_roles(healthroles[7],reason="attacked")
+			await attacked.add_roles(healthroles[6],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[6] in attacked.roles):
+			await attacked.remove_roles(healthroles[6],reason="attacked")
+			await attacked.add_roles(healthroles[5],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[5] in attacked.roles):
+			await attacked.remove_roles(healthroles[5],reason="attacked")
+			await attacked.add_roles(healthroles[4],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[4] in attacked.roles):
+			await attacked.remove_roles(healthroles[4],reason="attacked")
+			await attacked.add_roles(healthroles[3],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[3] in attacked.roles):
+			await attacked.remove_roles(healthroles[3],reason="attacked")
+			await attacked.add_roles(healthroles[2],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[2] in attacked.roles):
+			await attacked.remove_roles(healthroles[2],reason="attacked")
+			await attacked.add_roles(healthroles[1],reason="attacked")
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+		elif (healthroles[1] in attacked.roles):
+			await message.channel.send(f"{attacker.name} killed {attacked.name} with a stand!")
+			killed = attacked
+			await kill(killed, healthroles)
+
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def hamonattack(attacker, attacked, dmg, healthroles, message):
+	stand = discord.utils.get(message.guild.roles, id = 712718404901142528)
+	dead_visitor = discord.utils.get(message.guild.roles, id = 747460046333673573)
+	rof_armour = discord.utils.get(message.guild.roles, id = 747465352841134190)
+	pantarmour = discord.utils.get(message.guild.roles, id = 751637329088610365)
+	scared = discord.utils.get(message.guild.roles, id = 749429015621664790)
+	standattackcooldown = discord.utils.get(message.guild.roles, id = 749764783498199080)
+	hamonattackcooldown = discord.utils.get(message.guild.roles, id = 749767003379728466)
+	hamonhealcooldown = discord.utils.get(message.guild.roles, id = 749768438418898975)
+	enraged = discord.utils.get(message.guild.roles, id = 749824364618186844)
+	enraged_victim = discord.utils.get(message.guild.roles, id = 749824617648226326)
+
+	
+	if healthroles[0] in message.author.roles or healthroles[0] in message.mentions[0].roles or scared in message.author.roles:
+		return
+	if stand in attacked.roles:
+		await message.channel.send("attack the user, not the stand")
+		return
+	if enraged in attacked.roles:
+		message.channel.send("user is in a blind rage, you cannot damage them")
+		return
+	if enraged_victim in attacked.roles and enraged in attacker.roles:
+		dmg = dmg*2
+	for x in range(dmg):
+		if (healthroles[10] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[10],reason="attacked")
+			await attacked.add_roles(healthroles[9],reason="attacked")
+		elif (healthroles[9] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[9],reason="attacked")
+			await attacked.add_roles(healthroles[8],reason="attacked")
+		elif (healthroles[8] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[8],reason="attacked")
+			await attacked.add_roles(healthroles[7],reason="attacked")
+		elif (healthroles[7] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[7],reason="attacked")
+			await attacked.add_roles(healthroles[6],reason="attacked")
+		elif (healthroles[6] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[6],reason="attacked")
+			await attacked.add_roles(healthroles[5],reason="attacked")
+		elif (healthroles[5] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[5],reason="attacked")
+			await attacked.add_roles(healthroles[4],reason="attacked")
+		elif (healthroles[4] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[4],reason="attacked")
+			await attacked.add_roles(healthroles[3],reason="attacked")
+		elif (healthroles[3] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[3],reason="attacked")
+			await attacked.add_roles(healthroles[2],reason="attacked")
+		elif (healthroles[2] in attacked.roles):
+			await message.channel.send(f"{attacker.name} attacked {attacked.name} with a stand for {dmg} health!")
+			await attacked.remove_roles(healthroles[2],reason="attacked")
+			await attacked.add_roles(healthroles[1],reason="attacked")
+		elif (healthroles[1] in attacked.roles):
+			await message.channel.send(f"{attacker.name} killed {attacked.name} with hamon")
+			killed = attacked
+			await kill(killed, healthroles)
+	await message.channel.send(f"{attacker.name} attacked {attacked.name} with hamon for {dmg} health!")
+
+@commands.cooldown(1, 30, commands.BucketType.user)
+async def heal(healed, points, healthroles):
+	if healthroles[0] in healed.roles:
+		return
+	for x in range(points):
+		if (healthroles[10] in healed.roles):
+			points = 0
+			break
+		elif (healthroles[9] in healed.roles):
+			await healed.remove_roles(healthroles[9],reason="healed")
+			await healed.add_roles(healthroles[10],reason="healed")
+		elif (healthroles[8] in healed.roles):
+			await healed.remove_roles(healthroles[8],reason="healed")
+			await healed.add_roles(healthroles[9],reason="healed")
+		elif (healthroles[7] in healed.roles):
+			await healed.remove_roles(healthroles[7],reason="healed")
+			await healed.add_roles(healthroles[8],reason="healed")
+		elif (healthroles[6] in healed.roles):
+			await healed.remove_roles(healthroles[6],reason="healed")
+			await healed.add_roles(healthroles[7],reason="healed")
+		elif (healthroles[5] in healed.roles):
+			await healed.remove_roles(healthroles[5],reason="healed")
+			await healed.add_roles(healthroles[6],reason="healed")
+		elif (healthroles[4] in healed.roles):
+			await healed.remove_roles(healthroles[4],reason="healed")
+			await healed.add_roles(healthroles[5],reason="healed")
+		elif (healthroles[3] in healed.roles):
+			await healed.remove_roles(healthroles[3],reason="healed")
+			await healed.add_roles(healthroles[4],reason="healed")
+		elif (healthroles[2] in healed.roles):
+			await healed.remove_roles(healthroles[2],reason="healed")
+			await healed.add_roles(healthroles[3],reason="healed")
+		elif (healthroles[1] in healed.roles):
+			await healed.remove_roles(healthroles[1],reason="healed")
+			await healed.add_roles(healthroles[2],reason="healed")
+
+async def sleepheal(healed, healthroles, sleeping):
+	while True:
+		await asyncio.sleep(300)
+		if healthroles[10] in healed.roles:
+			await healed.remove_roles(sleeping, reason="healed to max")
+			break
+		if not sleeping in healed.roles:
+			break
+		elif (healthroles[9] in healed.roles):
+			await healed.remove_roles(healthroles[9],reason="healed")
+			await healed.add_roles(healthroles[10],reason="healed")
+		elif (healthroles[8] in healed.roles):
+			await healed.remove_roles(healthroles[8],reason="healed")
+			await healed.add_roles(healthroles[9],reason="healed")
+		elif (healthroles[7] in healed.roles):
+			await healed.remove_roles(healthroles[7],reason="healed")
+			await healed.add_roles(healthroles[8],reason="healed")
+		elif (healthroles[6] in healed.roles):
+			await healed.remove_roles(healthroles[6],reason="healed")
+			await healed.add_roles(healthroles[7],reason="healed")
+		elif (healthroles[5] in healed.roles):
+			await healed.remove_roles(healthroles[5],reason="healed")
+			await healed.add_roles(healthroles[6],reason="healed")
+		elif (healthroles[4] in healed.roles):
+			await healed.remove_roles(healthroles[4],reason="healed")
+			await healed.add_roles(healthroles[5],reason="healed")
+		elif (healthroles[3] in healed.roles):
+			await healed.remove_roles(healthroles[3],reason="healed")
+			await healed.add_roles(healthroles[4],reason="healed")
+		elif (healthroles[2] in healed.roles):
+			await healed.remove_roles(healthroles[2],reason="healed")
+			await healed.add_roles(healthroles[3],reason="healed")
+		elif (healthroles[1] in healed.roles):
+			await healed.remove_roles(healthroles[1],reason="healed")
+			await healed.add_roles(healthroles[2],reason="healed")
---
+++ main.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+
+from discord.ext.commands import Bot
+from discord.ext.commands import Cog, BucketType
+from discord.ext.commands import BadArgument
+from discord.ext.commands import command, cooldown
+import random
+import datetime
+import asyncio
+import discord
+from discord import Member, Embed
+
+import health
+
+intents = discord.Intents.all()
+
+with open("tokenfile", "r") as tokenfile:
+	token=tokenfile.read()
+
+client = discord.Client(intents=intents)
+
+async def attachments_to_files(attached,spoiler=False):
+	filelist = []
+	for i in attached:
+		file = await i.to_file()
+		filelist.insert(len(filelist),file)
+	return filelist
+
+prefix = "!"
+
+@client.event
+async def on_ready():
+	print("hello world!")
+
+@client.event
+async def on_message(message):
+	healthroles = [discord.utils.get(message.guild.roles, id = 714535509279637525), discord.utils.get(message.guild.roles, id = 714535481928319016), discord.utils.get(message.guild.roles, id = 714535460860330066), discord.utils.get(message.guild.roles, id = 714535436143558788), discord.utils.get(message.guild.roles, id = 714535405843775518), discord.utils.get(message.guild.roles, id = 714535379675644054), discord.utils.get(message.guild.roles, id = 714535348046266449), discord.utils.get(message.guild.roles, id = 714535324759621712), discord.utils.get(message.guild.roles, id = 714535298729640047), discord.utils.get(message.guild.roles, id = 714535185273847871), discord.utils.get(message.guild.roles, id = 714534974916919349)]
+	sleeping = discord.utils.get(message.guild.roles, id = 741552665892356147)
+	hamon = discord.utils.get(message.guild.roles, id = 713208239764013097)
+	standuser = discord.utils.get(message.guild.roles, id = 712718459431157888)
+	standAndHamon = discord.utils.get(message.guild.roles, id = 713253110771875860)
+	stand = discord.utils.get(message.guild.roles, id = 712718404901142528)
+	dead_visitor = discord.utils.get(message.guild.roles, id = 747460046333673573)
+	rof_armour = discord.utils.get(message.guild.roles, id = 747465352841134190)
+	pantarmour = discord.utils.get(message.guild.roles, id = 751637329088610365)
+	scared = discord.utils.get(message.guild.roles, id = 749429015621664790)
+	standattackcooldown = discord.utils.get(message.guild.roles, id = 749764783498199080)
+	hamonattackcooldown = discord.utils.get(message.guild.roles, id = 749767003379728466)
+	hamonhealcooldown = discord.utils.get(message.guild.roles, id = 749768438418898975)
+	enraged = discord.utils.get(message.guild.roles, id = 749824364618186844)
+	enraged_victim = discord.utils.get(message.guild.roles, id = 749824617648226326)
+	curse = discord.utils.get(message.guild.roles, id = 750130819099656284)
+
+	args = message.content.lower().replace(prefix, "")
+	argslist = args.split(" ")
+
+	if (message.channel.id == 712716066618605591):#general
+		if message.content.startswith('?echo'):
+			return
+		if message.content.startswith('?echo-edit'):
+			return
+		if message.content.startswith('sandman echo'):
+			return
+		text = message.content
+		channel = client.get_channel(737149672728297552)
+		images = await attachments_to_files(message.attachments,True)
+		await channel.send(content="**{0}**\n{1}".format(message.author.mention,text),files=images)
+	elif (message.channel.id == 754364505156223007):#memes
+		if message.content.startswith('?echo'):
+			return
+		if message.content.startswith('?echo-edit'):
+			return
+		if message.content.startswith('sandman echo'):
+			return
+		text = message.content
+		channel = client.get_channel(754376242777686047)
+		images = await attachments_to_files(message.attachments,True)
+		await channel.send(content="**{0}**\n{1}".format(message.author.mention,text),files=images)
+	elif (message.channel.id == 754364579177300068):#battlezone
+		if message.content.startswith('?echo'):
+			return
+		if message.content.startswith('?echo-edit'):
+			return
+		if message.content.startswith('sandman echo'):
+			return
+		text = message.content
+		channel = client.get_channel(754376278286663762)
+		images = await attachments_to_files(message.attachments,True)
+		await channel.send(content="**{0}**\n{1}".format(message.author.mention,text),files=images)
+	elif (message.channel.id == 730272782285537350): #stand-ideas
+		if message.content.startswith('?echo'):
+			return
+		if message.content.startswith('?echo-edit'):
+			return
+		if message.content.startswith('sandman echo'):
+			return
+		text = message.content
+		channel = client.get_channel(762741049525141514)
+		images = await attachments_to_files(message.attachments,True)
+		await channel.send(content="**{0}**\n{1}".format(message.author.mention,text),files=images)
+	elif (message.channel.id == 750761929810903103): #nsfw
+		if message.content.startswith('?echo'):
+			return
+		if message.content.startswith('?echo-edit'):
+			return
+		if message.content.startswith('sandman echo'):
+			return
+		text = message.content
+		channel = client.get_channel(762741097411248149)
+		images = await attachments_to_files(message.attachments,True)
+		await channel.send(content="**{0}**\n{1}".format(message.author.mention,text),files=images)
+
+	if message.author == client.user or message.channel.id == 747472960016875570:
+		return
+
+	if message.content.startswith(prefix):
+		if (argslist[0] == "sleep"):
+			if not sleeping in message.author.roles:
+				await message.channel.send("you begin to fall asleep")
+				await asyncio.sleep(5)
+				await message.channel.send("you fall asleep")
+				await message.author.add_roles(sleeping, reason="sleeping")
+				healed = message.author
+				await health.sleepheal(healed, healthroles, sleeping)
+			elif sleeping in message.author.roles:
+				await message.channel.send("you begin to wake up")
+				await asyncio.sleep(5)
+				await message.channel.send("you wake up")
+				await message.author.remove_roles(sleeping, reason="sleeping")
+
+	if hamon in message.author.roles or standAndHamon in message.author.roles:
+		if argslist[0] == "hamon":
+			if argslist[1] == "overdrive":
+				if message.mentions == []:
+					await message.channel.send("you need to ping a user to attack")
+					return
+				if rof_armour in message.mentions[0].roles:
+					attacker = message.mentions[0]
+					attacked = message.author
+				else:
+					attacker = message.author
+					attacked = message.mentions[0]
+				dmg = 1
+				await health.hamonattack(attacker, attacked, dmg, healthroles, message)
+			if argslist[1] == "heal":
+				healed = message.author
+				points = random.randrange(0, 2)
+				await health.heal(healed, points, healthroles)
+
+	if standuser in message.author.roles or standAndHamon in message.author.roles:
+		if (argslist[0] == "stand"):
+			if (argslist[1] == "attack"):
+				if message.mentions == []:
+					await message.channel.send("you need to ping a user to attack")
+					return
+				if rof_armour in message.mentions[0].roles:
+					attacker = message.mentions[0]
+					attacked = message.author
+				else:
+					attacker = message.author
+					attacked = message.mentions[0]
+				dmg = 1
+				await health.standattack(attacker, attacked, dmg, healthroles, message)
+
+client.run(token)
